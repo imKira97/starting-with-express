bff09c4d84b7e17997b70813c3f3d4b89872fcd8
imKira97 <codingshiva97>
2022-12-25 21:06:33 +0530

    path and body parser

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 // const http=require('http'); no need when doing app.listen()
 const express=require('express');
 
+const bodyPraser=require('body-parser');
+
 //we can create express application and store it in a constant  by running express as a function
 //this will initialize a new object this express will store and manage a lot of things for us 
 //also this app  constant act as valid request handler which we can pass to createServer function
 const app=express();
 
 
+/*
 //use() allows us to add a new middleware function . It accepts an array of so-called request handlers here and it has some other use case too
 app.use((request,response,next)=>{
     console.log('in the middleware ');
@@ -18,7 +21,7 @@ next is actually a function, a function that will be passed to this function by 
 we are passing next()-as a argument
 when we execute next() it pass to another middleware 
 if we dont write next() it dies
-*/
+
 
 //the function which we have pass will be executed for every incoming request and this 
 app.use((request,response,next)=>{
@@ -28,10 +31,54 @@ app.use((request,response,next)=>{
     this allow us to attach a body which is of type any 
     send automatically sets header we dont have to write setHeader 
     also we setHeader manually if we want to 
-    */
+    
     response.send('<h1>Hello from express js response</h1>');
 
 
+})
+
+*/
+
+/*
+app.use('/',(req,res,next)=>{
+    console.log('always run');
+    //res.send('<h1>forr all</h1>');
+    /*
+    we cant use next() with res.send 
+    because we dont want to send more than one response  and it will throw an error
+    
+    next();
+})
+*/
+
+
+/*
+bodyparser is added before routing because the parsing of body should be done no matter where request ends up
+
+it registers a middleware,
+so this parses such a function here in the end even though we can't see it and this package will in the
+end, in this middleware function call next in the end, so that the request also reaches our middleware
+but before it does that, it will do that whole request body parsing 
+
+*/
+app.use(bodyPraser.urlencoded({extended:false}))
+
+// path is determine and result is shown
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><label>Add product: </label> <input type="text" name="title"><br> <label>quantity: </label> <input type="number" name="quantity"> <br><button type="submit">Add Product</button> </form>')
+})
+
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    /*
+    before importing bodyParser we are getting undefined 
+    but now we will get object in key value pair
+    */
+    res.redirect('/');//this will redirect from product page to home page
+})
+
+app.use('/',(req,res,next)=>{
+    res.send('<h1>home</h1>')
 })
 
 
