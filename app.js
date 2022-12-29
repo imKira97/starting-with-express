0867e2d4dd296f0b884fcb2f16571efecfaa0aac
imKira97 <codingshiva97>
2022-12-29 13:29:26 +0530

    added controller with errorpage

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const errorHandle=require('./controller/errorHandle')
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -11,14 +12,13 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(errorHandle.get404);
 
 app.listen(3000);
