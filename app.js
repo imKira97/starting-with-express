b93039893fd8f1fddb0955e27de5b20e5470149c
imKira97 <codingshiva97>
2022-12-30 11:26:25 +0530

    project before dynamic routes

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const errorController = require('./controllers/error');
+
 const app = express();
-const errorHandle=require('./controller/errorHandle')
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -12,13 +13,12 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use(errorHandle.get404);
+app.use(errorController.get404);
 
 app.listen(3000);
--- util/path.js
+++ util/path.js
@@ -1,3 +1,3 @@
-const path=require('path');
-module.exports=path.dirname(require.main.filename);
-//this will return main dir where our main app.js file is there
+const path = require('path');
+
+module.exports = path.dirname(process.mainModule.filename);
